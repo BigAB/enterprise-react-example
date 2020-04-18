396df2a2015418b14d1539bb602ad21f7405a04a
Adam L Barrett <adamlbarrett@hotmail.com>
2020-04-18 17:15:14 -0600

    initial commit

---
+++ apps/initiative-management/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  name: 'initiative-management',
+  preset: '../../jest.config.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
+    '^.+\\.[tj]sx?$': 'ts-jest'
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'html'],
+  coverageDirectory: '../../coverage/apps/initiative-management'
+};
